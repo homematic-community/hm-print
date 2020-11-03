8625e5f3bb38e18a639146e7c3d48eca61bf36b0
Jens Maus <mail@jens-maus.de>
2020-11-03 09:19:59 +0100

    adapted certain things to match this repository style again.

--- generate_img.sh
+++ generate_img.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-COPYFILE_DISABLE=1; export COPYFILE_DISABLE
-rm -f *.tar.gz
-
 mkdir -p tmp/addon/
 cp -a update_script tmp/
 cp -a rc.d tmp/
@@ -10,11 +7,9 @@ cp -a VERSION tmp/addon/
 cd tmp
 chmod +x update_script
 
-find . -name ".DS_Store" -exec rm -rf {} \;
-find . -name "._*" -exec rm -rf {} \;
-dot_clean .
+[ -f /usr/sbin/dot_clean ] && dot_clean .
 
-tar  -zcf ../hm-print-$(cat ../VERSION).tar.gz * 
+COPYFILE_DISABLE=1 tar --owner=root --group=root --exclude=.DS_Store -czvf ../hm-print-$(cat ../VERSION).tar.gz *
 
 cd ..
 rm -rf tmp
